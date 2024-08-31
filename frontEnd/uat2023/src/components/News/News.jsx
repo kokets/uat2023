62ed5ce761579ad8b35efc718b6e85f28bf7d54b
kokets <koketsoforever@gmail.com>
2024-08-31 18:30:51 +0200

    update

--- frontEnd/uat2023/src/components/News/News.jsx
+++ frontEnd/uat2023/src/components/News/News.jsx
@@ -45,58 +45,58 @@ const News = () => {
       id: 1,
       image: image15,
       title:
-        " Mother Nature at Ekurhuleni Duduza celebrating women's month having various sports and entertainment activities",
+        " Mother Nature at Ekurhuleni Duduza celebrating ",
       date: "19 August 2024",
     },
     {
       id: 2,
       image: image16,
       title:
-        "   UAT General Secretary Japhter Baloyi visited limpopo to celebrate women's month with mother nature",
+        "UAT General Secretary Japhter Baloyi visited ",
       date: "19 August 2024",
     },
     {
       id: 3,
       image: image17,
       title:
-        "UAT delegates National Spokesperson Doer Mighty Mabule and National Chairperson Doer Kagiso Diphoko attended the 1st Anniversary of African Economic Freedom (AEF ) to offer message of Support on behalf of UAT Today In KwaZulu Natal , Mtubatuba",
+        "UAT delegates National Spokesperson Doer  ",
       date: "19 August 2024",
     },
     {
       id: 4,
       image: image4,
       title:
-        "  United Africans Transformation and Tau Project at Ga-Phaahla launching Borehole Construction",
+        "  United Africans Transformation and Tau Project ",
       date: "19 August 2024",
     },
     {
       id: 5,
       image: image19,
       title:
-        "  Ekurhuleni Doers was giving away sanitary napkins at H.B Nyathi Secondary School ",
+        "Ekurhuleni Doers was giving away sanitary ",
       date: "19 August 2024",
     },
-    {
-      id: 6,
-      image: image10,
-      title:
-        "UAT delegation to official opening of of the seventh Limpopo Legislature 2024",
-      date: "19 August 2024",
-    },
-    {
-      id: 7,
-      image: image15,
-      title:
-        "Dr. Benjamin Manasoe, a member of the NEC and UAT, embarked on a social uplifting program by giving back to the primary school he attended. This gesture of appreciation aimed to solidify the relationship between the school and UAT.",
-      date: "19 August 2024",
-    },
-    {
-      id: 8,
-      image: image15,
-      title:
-        " Mother Nature at Ekurhuleni Duduza celebrating women's month having various sports and entertainment activities",
-      date: "19 August 2024",
-    },
+    // {
+    //   id: 6,
+    //   image: image10,
+    //   title:
+    //     "UAT delegation to official opening of of the seventh Limpopo Legislature 2024",
+    //   date: "19 August 2024",
+    // },
+    // {
+    //   id: 7,
+    //   image: image15,
+    //   title:
+    //     "Dr. Benjamin Manasoe, a member of the NEC and UAT, embarked on a social uplifting program by giving back to the primary school he attended. This gesture of appreciation aimed to solidify the relationship between the school and UAT.",
+    //   date: "19 August 2024",
+    // },
+    // {
+    //   id: 8,
+    //   image: image15,
+    //   title:
+    //     " Mother Nature at Ekurhuleni Duduza celebrating women's month having various sports and entertainment activities",
+    //   date: "19 August 2024",
+    // },
     // Add more featured news items
   ];
 
@@ -116,8 +116,7 @@ const News = () => {
   return (
     <div className="newsSection">
       <div className="news-Title">
-          <h1>LATEST NEWS</h1>
-     
+        <h1>LATEST NEWS</h1>
       </div>
 
       <div className="news-Cards">
@@ -132,26 +131,27 @@ const News = () => {
           <p className="date"></p>
         </div> */}
         {news.map((newsItem) => (
-            <motion.div
-              key={newsItem.id}
-              className="news-Card"
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-              viewport={{ once: true, amount: 0.3 }} // Trigger animation when 30% of the card is in view
-            >
-              <img src={newsItem.image} alt={`News Image ${newsItem.id}`} />
-              <div className="news-Content">
-              <h1>{newsItem.title}</h1>
-              <hr />
-              <p className="date">{newsItem.date}</p>
-              </div>
-        
-            </motion.div>
-          ))}
-
+          <motion.div
+            key={newsItem.id}
+            className="news-Card"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: true, amount: 0.3 }} // Trigger animation when 30% of the card is in view
+          >
+            <img src={newsItem.image} alt={`News Image ${newsItem.id}`} />
+            <div className="topStory">
+              <h1>TOP STORY</h1>
+            </div>
+            <div className="news-Content">
+              <p className="news-Items-Content">{newsItem.title}</p>
+              <div className="content-Separator"></div>
+              <div className="content-Separator"></div>
+              <p className="news-Items-Content">{newsItem.date}</p>
+            </div>
+          </motion.div>
+        ))}
       </div>
-
     </div>
   );
 };
