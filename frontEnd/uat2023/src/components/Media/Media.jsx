23455af47f797a8d878999158e8ba334c83ee237
kokets <koketsoforever@gmail.com>
2024-09-02 20:51:02 +0200

    update

--- frontEnd/uat2023/src/components/Media/Media.jsx
+++ frontEnd/uat2023/src/components/Media/Media.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import SimpleImageSlider from "react-simple-image-slider";
 import "./Media.scss";
 import {
@@ -15,7 +15,12 @@ import {
   image11,
   image12,
 } from "./assets/index.jsx";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Media = () => {
   const images = [
@@ -32,25 +37,74 @@ const Media = () => {
     { url: image11 },
     { url: image12 },
   ];
+  const galleryRef = useRef(null);
+
+  // Scroll left
+  const scrollLeft = () => {
+    galleryRef.current.scrollBy({
+      left: -450, // Adjust scroll distance as needed (same as image width)
+      behavior: "smooth",
+    });
+  };
+
+  // Scroll right
+  const scrollRight = () => {
+    galleryRef.current.scrollBy({
+      left: 450, // Adjust scroll distance as needed (same as image width)
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="mediaSection">
-      <div className="media">
-        <div>
-          <h1>Media Gallery</h1>
+      <div className="">
+        <h1>Media Gallery</h1>
+      </div>
+      <div className="mediaGalleryWrapper">
+        <div class="nav-icon" onClick={scrollLeft}>
+          <FontAwesomeIcon icon={faChevronLeft} />
         </div>
-
-        <div className="mediaGallery">
-          {/* <SimpleImageSlider
-            width={996}
-            height={504}
-            images={images}
-            showBullets={true}
-            showNavs={true}
-          /> */}
-  
-        </div>
-        <div className="mediaImage">
+        {/* <div className="mediaGallery" ref={galleryRef}> */}
         <motion.div
+          className="mediaGallery"
+          ref={galleryRef}
+          whileTap={{ cursor: "grabbing" }}
+          drag="x"
+          dragConstraints={{ left: -3000, right: 0 }} // Adjust constraints based on content width
+        >
+          {images.map((image, index) => (
+          <div className="galleryImage" key={index}>
+            <img src={image.url} alt={`Gallery ${index + 1}`} />
+          </div>
+        ))}
+          {/* <div className="galleryImage">
+            <img src={image12}></img>
+          </div>
+          <div className="galleryImage">
+            <img src={image8}></img>
+          </div>
+          <div className="galleryImage">
+            <img src={image2}></img>
+          </div>
+          <div className="galleryImage">
+            <img src={image3}></img>
+          </div>
+          <div className="galleryImage">
+            <img src={image4}></img>
+          </div>
+          <div className="galleryImage">
+            <img src={image5}></img>
+          </div> */}
+        </motion.div>
+        {/* </div> */}
+        <div class="nav-icon" onClick={scrollRight}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+      </div>
+
+      <div className="media">
+        <div className="mediaImage">
+          <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
@@ -65,8 +119,8 @@ const Media = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
-           </motion.div>
-           <motion.div
+          </motion.div>
+          <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
@@ -80,7 +134,7 @@ const Media = () => {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
-         </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
